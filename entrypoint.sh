5ab8dfd9b3fd24dcc1a16b882c0f7820c409da0d
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2024-07-20 15:01:52 +0300

    обновлен энтрипоинт для мейна

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "Loading Docker image go-runner"
+
 # Проверяем доступность Docker Daemon
 if ! docker info > /dev/null 2>&1; then
     echo "Docker Daemon is not running. Please ensure Docker Daemon is running."
@@ -11,6 +13,8 @@ if ! docker load -i /go-runner.tar; then
     echo "Failed to load Docker image go-runner"
     exit 1
 fi
+
+echo "Docker image go-runner loaded successfully"
 
 # Запускаем основное приложение
 exec "$@"
