b13ff4adb9c8f076a7d0bba3e01eb9ddee947f72
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2024-07-20 14:35:47 +0300

    создан энтрипоинт для главного конетейнера

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Проверяем доступность Docker Daemon
+if ! docker info > /dev/null 2>&1; then
+    echo "Docker Daemon is not running. Please ensure Docker Daemon is running."
+    exit 1
+fi
+
+# Загружаем образ go-runner
+if ! docker load -i /go-runner.tar; then
+    echo "Failed to load Docker image go-runner"
+    exit 1
+fi
+
+# Запускаем основное приложение
+exec "$@"
