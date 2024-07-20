6e9789b5bf095bd770ab3f4a7afbe62d9402e5a0
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2024-07-20 14:35:54 +0300

    обновлен мейн

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"github.com/algrvvv/go-sandbox/src"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	serv := src.GetNewServer()
+	log.Println("Starting server on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", serv))
 }
