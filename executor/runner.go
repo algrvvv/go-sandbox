878d20daa660ec63a2a58266fc52c71e855b20e4
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2024-07-20 16:33:30 +0300

    самую малось изменен `runner`

--- executor/runner.go
+++ executor/runner.go
@@ -8,12 +8,11 @@ import (
 )
 
 func main() {
-	log.Println("[Runner] start new runner")
 	codePath := os.Args[1]
-	log.Printf("[Runner] path: %s", codePath)
 	code, err := os.ReadFile(codePath)
 	if err != nil {
-		log.Fatalf("failed to read code: %v", err)
+		log.Printf("failed to read code: %v", err)
+		return
 	}
 
 	cmd := exec.Command("go", "run", "-")
@@ -22,6 +21,6 @@ func main() {
 	cmd.Stderr = os.Stderr
 
 	if err = cmd.Run(); err != nil {
-		log.Fatalf("failed to run code: %v", err)
+		log.Printf("failed to run code: %v", err)
 	}
 }
