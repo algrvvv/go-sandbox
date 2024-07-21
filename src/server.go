f4ca49d4ab370b014008e5b9d006600e153e943d
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2024-07-21 13:01:18 +0300

    изменены маршруты с заделом на онлайн

--- src/server.go
+++ src/server.go
@@ -8,7 +8,9 @@ func GetNewServer() *http.ServeMux {
 	serv := http.NewServeMux()
 
 	serv.HandleFunc("GET /", indexHandler)
-	serv.HandleFunc("POST /run", runHandler)
+	serv.HandleFunc("GET /offline", indexHandler)
+	serv.HandleFunc("GET /online", indexHandler)
+	serv.HandleFunc("POST /offline/run", runHandler)
 	serv.HandleFunc("GET /new", newSessionHandler)
 	serv.HandleFunc("GET /ws", wsHandler)
 
