b9fc3c7ac899e3bea8ad1d060209988c272efcf3
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2024-09-12 13:38:07 +0300

    добавлен файл для запроса на форматирование кода

---
+++ static/js/gofmt.js
@@ -0,0 +1,16 @@
+function gofmt() {
+    const btn = document.getElementById('fmt-button');
+    btn.innerHTML = 'Formating...';
+    btn.disabled = true;
+
+    const code = editor.getValue();
+    axios.post("/gofmt", {code: code}).then(res => {
+        console.log("response after gofmt", res)
+        editor.setValue(res.data.code);
+    }).catch(err => {
+        console.error("failed to format file", err)
+    }).finally(() => {
+        btn.innerHTML = 'Format';
+        btn.disabled = false;
+    })
+}
