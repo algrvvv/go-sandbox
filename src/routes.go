f35dc06196549f20c8c16c39a229d018c06488f4
p4xt3r <121817930+algrvvv@users.noreply.github.com>
2024-09-12 13:36:37 +0300

    добавлен совместный запуск кода, а также форматирование файла

--- src/routes.go
+++ src/routes.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
 	"sync"
 	"text/template"
 
@@ -92,18 +94,62 @@ func onlineHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func runHandler(w http.ResponseWriter, r *http.Request) {
-	code := r.FormValue("code")
-	res := executeUserCode(code)
-
-	tmp, err := template.ParseFiles("templates/index.html")
+func onlineRunHandler(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Session string `json:"session"`
+		Uid     string `json:"uid"`
+		Code    string `json:"code"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&data)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		logger.Error("failed to parse request: "+err.Error(), err)
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte(err.Error()))
+		return
 	}
 
-	if err = tmp.Execute(w, res); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	s := SessionID(data.Session)
+	if _, exists := sessions[s]; !exists {
+		logger.Warn("session not found")
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte("session not found"))
+		return
 	}
+
+	if data.Uid == "" {
+		logger.Warn("u is empty")
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte("u is empty"))
+		return
+	}
+
+	res := executeUserCode(data.Code)
+	jsonData, err := json.Marshal(map[string]interface{}{
+		"type": "console",
+		// TODO планировалось, что первоначальному пользователю будет сразу же ответом приходить,
+		// но пока что будет отправляться тоже по вебсокетам.
+		"exceptedUser": data.Uid,
+		"data":         res,
+	})
+	if err != nil {
+		logger.Error("failed to marshal response: "+err.Error(), err)
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write([]byte(err.Error()))
+		return
+	}
+
+	// TODO понаблюдать за поведением, наличием дедлоков
+	mu.Lock()
+	for _, c := range sessions[s] {
+		err = c.WriteMessage(websocket.TextMessage, jsonData)
+		if err != nil {
+			logger.Error("failed to write message: "+err.Error(), err)
+		}
+	}
+	mu.Unlock()
+
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("Success execute"))
 }
 
 func newOnlineHandler(w http.ResponseWriter, r *http.Request) {
@@ -117,8 +163,8 @@ func connectOnlineHandler(w http.ResponseWriter, r *http.Request) {
 	s := SessionID(r.URL.Query().Get("s"))
 	if _, exists := sessions[s]; exists {
 		uid := uuid.New().String()
-		//ss := append(*currentSessions, Session{Uid: uid})
-		//sessions[s] = &ss
+		// ss := append(*currentSessions, Session{Uid: uid})
+		// sessions[s] = &ss
 		url := fmt.Sprintf("/online?s=%s&u=%s", s, uid)
 		http.Redirect(w, r, url, http.StatusFound)
 	} else {
@@ -130,8 +176,8 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("new connection")
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
+		logger.Error("failed to upgrade connection: "+err.Error(), err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Printf("online err: %v", err)
 		return
 	}
 	defer conn.Close()
@@ -142,13 +188,14 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	)
 
 	if sessionID == "" || uid == "" {
+		logger.Error("Missing session id or uid", nil)
 		http.Error(w, "Missing session id or uid", http.StatusBadRequest)
 		return
 	}
 
-	//if _, ok := sessions[sessionID]; !ok {
+	// if _, ok := sessions[sessionID]; !ok {
 	//	sessions[sessionID] = make([]*websocket.Conn, 0)
-	//}
+	// }
 
 	mu.Lock()
 	sessions[sessionID] = append(sessions[sessionID], conn)
@@ -174,6 +221,7 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	for {
 		_, message, err := conn.ReadMessage()
 		if err != nil {
+			logger.Error("failed to read message: "+err.Error(), err)
 			log.Printf("ws read err: %v", err)
 			break
 		}
@@ -183,7 +231,7 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 			if c != conn {
 				err = c.WriteMessage(websocket.TextMessage, message)
 				if err != nil {
-					log.Printf("ws write err: %v", err)
+					logger.Error("failed to write message: "+err.Error(), err)
 				}
 			}
 		}
@@ -191,6 +239,57 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func handleConnection(conn *websocket.Conn) {
+func goFmtHandler(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Code string `json:"code"`
+	}
 
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		logger.Error("failed to parse request: "+err.Error(), err)
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = w.Write([]byte(err.Error()))
+		return
+	}
+
+	tmp, err := os.CreateTemp("", "gofmt_*.go")
+	if err != nil {
+		logger.Error("failed to create temp file: "+err.Error(), err)
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write([]byte(err.Error()))
+		return
+	}
+	defer func() { _ = os.Remove(tmp.Name()) }()
+
+	_, _ = tmp.Write([]byte(data.Code))
+
+	cmd := exec.Command("gofmt", "-w", tmp.Name())
+	if err = cmd.Run(); err != nil {
+		logger.Error("failed to run gofmt: "+err.Error(), err)
+	}
+
+	cmd = exec.Command("goimports", "-w", tmp.Name())
+	if err = cmd.Run(); err != nil {
+		logger.Error("failed to run goimports: "+err.Error(), err)
+	}
+
+	final, err := os.ReadFile(tmp.Name())
+	if err != nil {
+		logger.Error("failed to read final result file: "+err.Error(), err)
+		return
+	}
+
+	jsonData, err := json.Marshal(map[string]interface{}{
+		"status": true,
+		"code":   string(final),
+	})
+	if err != nil {
+		logger.Error("failed to marshal response: "+err.Error(), err)
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(jsonData)
 }
